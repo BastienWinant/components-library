4b5b1654825bcaabd15d01c293082344621b85b1
Bastien Winant <bastien.winant@gmail.com>
2025-01-07 18:31:08 +0100

    create a sepate css file for badge component

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,8 +1,9 @@
+import './Badge.css'
 import React from "react"
 
 export default function Badge({children}) {
   return (
-    <div className="badge badge-square">
+    <div className="badge badge-rectangle badge-regular">
       <p className="badge-text">{children}</p>
     </div>
   )
