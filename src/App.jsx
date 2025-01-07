427564a38147ea3bcb28a23c97800a6125c55b02
Bastien Winant <bastien.winant@gmail.com>
2025-01-07 19:14:10 +0100

    pass classnames as props

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,21 @@ import Badge from "./components/Badge/Badge"
 function App() {
 
   return (
-    <Badge>badge</Badge>
+    <main>
+      <section>
+        <header>
+          <h2>badges</h2>
+        </header>
+        <div>
+          <div>
+            <Badge className="square">badge</Badge>
+          </div>
+          <div>
+            <Badge className="pill">badge</Badge>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,9 +1,10 @@
 import './Badge.css'
 import React from "react"
+import clsx from 'clsx'
 
-export default function Badge({children}) {
+export default function Badge({children, className}) {
   return (
-    <div className="badge badge-rectangle badge-regular">
+    <div className={clsx("badge", className)}>
       <p className="badge-text">{children}</p>
     </div>
   )
