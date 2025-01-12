41908a24ba20ae9e184de26122ce1bf3602d6106
Bastien Winant <bastien.winant@gmail.com>
2025-01-12 20:37:22 +0100

    code cleanup

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -5,16 +5,6 @@ export default function Testimonial({children, imgSrc}) {
       <div className="testimonial-container">
         <img className="testimonial-img" src={imgSrc} alt={imgAltText} />
         {children}
-        {/* <figure className="testimonial-body">
-          <img src={blockQuoteIcon} alt="Quotation mark" />
-          <blockquote className="testimonial-quote">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, maxime aut. Ullam in ipsa delectus aut placeat deleniti rerum consequuntur beatae, nostrum velit, eos, iusto voluptas? Sunt sit at ea?
-          </blockquote>
-          <figcaption className="testimonial-caption">
-            <p className="testimonial-name">May Andersons</p>
-            <p className="testimonial-title">Workcation, CTO</p>
-          </figcaption>
-        </figure> */}
       </div>
     </article>
   )
