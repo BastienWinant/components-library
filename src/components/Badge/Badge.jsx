1d6149a970567c5b68db5c651a21b7d235641af5
Bastien Winant <bastien.winant@gmail.com>
2025-01-13 01:51:35 +0100

    pass and apply color & shape props

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,8 +1,11 @@
 import './Badge.css'
+import clsx from 'clsx'
 
-export default function Badge({children}) {
+export default function Badge({children, color, shape}) {
+  let className = clsx('badge', `${color}-light`, shape)
+
   return (
-    <div className="badge">
+    <div className={className}>
       <p className="badge-text">{children}</p>
     </div>
   )
